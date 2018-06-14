0ba353f5e733685e7a45a3cab2e52ec2223b985e
Juozas Beniusis <beniusij@gmail.com>
2018-06-14 16:53:22 +0100

    Working on tests. Stuck with the first two failures.

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,17 @@ Route::get('/', function () {
     return view('welcome', ['links' => $links]);
 });
 
-Route::get('/submit', function () {
+use Illuminate\Http\Request;
+
+Route::get('/submit', function (Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'url' => 'required|url|max:255',
+        'description' => 'required|max:255',
+    ]);
+
+    $link = tap(new App\Link($data))->save();
+
    return view('submit');
 });
 
